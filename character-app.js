30cdf1a8c4aa3867078862b06a8beb67fb302c36
mbolger8 <mjbolger462@gmail.com>
2025-03-16 23:44:22 -0400

    check in

--- character-app.js
+++ character-app.js
@@ -82,4 +82,19 @@ export class CharacterApp extends DDDSuper(I18NMixin(LitElement)) {
   }
 }
 
+async function getData() {
+  const url = "https://example.org/products.json";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    console.log(json);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
 globalThis.customElements.define(CharacterApp.tag, CharacterApp);
